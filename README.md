33ccbe7879832b63db7bc81187d2ed9aff08b291
James Bentley <jameswillisbentley@gmail.com>
2021-09-17 11:15:06 -0700

    git

---
+++ README.md
@@ -0,0 +1,8 @@
+# simon
+Created Simon Game, a memory game that utilizes queues where the user is given a pattern of colors and returns the colors after in order.
+
+The game will tell you what colors to click on over five rounds, with the amount of random colors incrementing per round. 
+
+The round number and colors will show up at the bottom of the screen after the "Start Game" button is pressed. 
+
+You will have 3 seconds to click each color
---
+++ project.js
@@ -0,0 +1,101 @@
+const queue = [];
+const validColors = ["green","red","yellow","blue"];
+var color;
+var check;
+var audio = new Audio()
+
+function buttonClick(id)
+{
+    color = id;
+}
+
+function empty(queue)
+{
+    return queue.length == 0;
+}
+
+function createGame(colorlist)
+{
+    while(!empty(queue))
+    {
+        queue.shift();
+    }
+    var colorArray = colorlist.split(" ");
+    for(let i = 0; i < colorArray.length; i++)
+    {
+        queue.push(colorArray[i]);
+    }
+}
+
+function getNextColor()
+{
+    return queue.shift();
+}
+
+function sleep(ms)
+{
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function doRound(round)
+{
+    var text = document.getElementById('output');
+    for(let i = 1; i <= round; i++) //allows for input of color for each new round
+    {
+        await sleep(3500);
+        text.innerText = round - i + " color(s) left!";
+        check = getNextColor();
+        var input = color;
+        if(input != check)
+        {
+            return false;
+        }
+        else if (i == round)
+        {
+            return true;
+        }
+    }
+} 
+
+async function game()
+{
+    var text = document.getElementById('output');
+    var winCount = 0;
+    var color;
+
+    for(let round = 1; round <= 5; round++)
+    {
+        text.innerText = "Round " + round;
+        var colorlist = "";
+        for(let i = 1; i <= round; i++) //chooses random colors
+        {
+            colorlist += validColors[((Math.random() * 4.0) | 0)] + " ";
+        }
+        createGame(colorlist);
+        await sleep(2500);
+        text.innerText = "Press the colors in order!\n\n" + queue;
+        
+        const wonRound = await doRound(round); //does individual round
+
+        if(wonRound == true) //checks for win condition
+        {
+            text.innerText = "Yay!";
+            winCount++;
+        }
+        else
+        {
+            text.innerText = "WRONG";
+            await sleep(1000);
+            break;
+        }
+    }
+
+    if(winCount == 5) //checks for final win
+    {
+        text.innerText = "GAME WON!!! Congratulations!";
+    }
+    else
+    {
+        text.innerText = "GAME OVER";
+    }
+}
